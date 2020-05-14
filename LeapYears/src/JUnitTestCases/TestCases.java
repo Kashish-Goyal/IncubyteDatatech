8c54c16823bd0d834a1a71e94d2c7c3bd26e0c84
kashish <kashish.goyal@moonshinetechonology.com>
2020-05-14 15:35:34 +0530

    added test cases for TDD

--- LeapYears/src/JUnitTestCases/TestCases.java
+++ LeapYears/src/JUnitTestCases/TestCases.java
@@ -4,11 +4,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import Solutions.LeapYear;
+
 class TestCases {
 
+	LeapYear ly = new LeapYear();
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testLeapYears() {
+		assertTrue(ly.checkLeapYear(2016));
+		assertTrue(ly.checkLeapYear(2020));
+		assertFalse(ly.checkLeapYear(2018));
+		assertTrue(ly.checkLeapYear(2017));
+		assertTrue(ly.checkLeapYear(2000));
+		assertTrue(ly.checkLeapYear(20000));
+		assertTrue(ly.checkLeapYear(18000));
+		try{
+			ly.checkLeapYear(-100);
+			fail("Exception not handled");
+		}catch(Exception e){
+		}
+		try{
+			assertTrue(ly.checkLeapYear(0));
+			fail("Exception not handled");
+		}catch(Exception e) {
+			
+		}
+		assertFalse(ly.checkLeapYear(19000));	
 	}
 
 }
--- LeapYears/src/Solutions/LeapYear.java
+++ LeapYears/src/Solutions/LeapYear.java
@@ -1,5 +1,9 @@
 package Solutions;
 
 public class LeapYear {
+	
+	public boolean checkLeapYear(int year) {
+		return false;
+	}
 
 }
